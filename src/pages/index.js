3113193f491bf364ad21b8e14a75c3f41a2b514c
Michael Uloth <hello@michaeluloth.com>
2018-11-13 00:35:21 -0500

    Move tutorials with no dates to the bottom of the list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 function IndexPage({ data }) {
-  const tutorials = data.tutorials.edges
+  // Move tutorials with no date to the end of the list
+  const tutorials = data.tutorialsWithDates.edges.concat(
+    data.tutorialsWithoutDates.edges
+  )
 
   // TODO: replace these runtime calculations with detailed YAML version once we're compiling it at build time
 
@@ -103,7 +106,27 @@ function IndexPage({ data }) {
 
 export const query = graphql`
   query {
-    tutorials: allTutorialsYaml(sort: { fields: [date], order: DESC }) {
+    tutorialsWithDates: allTutorialsYaml(
+      filter: { date: { ne: null } }
+      sort: { fields: [date], order: DESC }
+    ) {
+      edges {
+        node {
+          title
+          link
+          format
+          date(formatString: "MMM DD, YYYY")
+          length
+          authors
+          source
+          topics
+        }
+      }
+    }
+    tutorialsWithoutDates: allTutorialsYaml(
+      filter: { date: { eq: null } }
+      sort: { fields: [title], order: ASC }
+    ) {
       edges {
         node {
           title
