0240605da1f9f539ff5d411099de07cc21fc2997
Michael Uloth <hello@michaeluloth.com>
2018-12-05 14:46:11 -0500

    Add missing filter combos and scroll to top on search

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -3,14 +3,19 @@ function Directory({ tutorials, formats, topics, authors, sources }) {
   const [topic, setTopic] = useState(null)
   const [author, setAuthor] = useState(null)
   const [source, setSource] = useState(null)
-  const [query, setQuery] = useState(null)
+  const [query, setQuery] = useState(``)
   const searchInput = useRef()
 
   // On the first render, focus the search input
   useEffect(() => searchInput.current.focus(), [])
 
+  function handleQuery(e) {
+    setQuery(e.target.value)
+    window.scrollTo(0, 0)
+  }
+
   // Filter the visible tutorials based on the active filters and/or search query
-  const filteredTutorials = tutorials.filter(({ node: tutorial }) => {
+  let filteredTutorials = tutorials.filter(({ node: tutorial }) => {
     let isFormatMatch = false
     let isTopicMatch = false
     let isAuthorMatch = false
@@ -41,6 +46,15 @@ function Directory({ tutorials, formats, topics, authors, sources }) {
       if (format && topic && author && source) {
         return isFormatMatch && isTopicMatch && isAuthorMatch && isSourceMatch
       }
+      if (format && !topic && author && !source) {
+        return isFormatMatch && isAuthorMatch
+      }
+      if (format && !topic && author && source) {
+        return isFormatMatch && isAuthorMatch && isSourceMatch
+      }
+      if (format && !topic && !author && source) {
+        return isFormatMatch && isSourceMatch
+      }
 
       // Remaining topic combos
       if (!format && topic && !author && !source) {
@@ -48,6 +62,9 @@ function Directory({ tutorials, formats, topics, authors, sources }) {
       }
       if (!format && topic && author && !source) {
         return isTopicMatch && isAuthorMatch
+      }
+      if (!format && topic && author && source) {
+        return isTopicMatch && isAuthorMatch && isSourceMatch
       }
       if (!format && topic && !author && source) {
         return isTopicMatch && isSourceMatch
@@ -106,6 +123,15 @@ function Directory({ tutorials, formats, topics, authors, sources }) {
           isQueryMatch
         )
       }
+      if (format && !topic && author && !source) {
+        return isFormatMatch && isAuthorMatch && isQueryMatch
+      }
+      if (format && !topic && author && source) {
+        return isFormatMatch && isAuthorMatch && isSourceMatch && isQueryMatch
+      }
+      if (format && !topic && !author && source) {
+        return isFormatMatch && isSourceMatch && isQueryMatch
+      }
 
       // Remaining topic combos
       if (!format && topic && !author && !source) {
@@ -113,6 +139,9 @@ function Directory({ tutorials, formats, topics, authors, sources }) {
       }
       if (!format && topic && author && !source) {
         return isTopicMatch && isAuthorMatch && isQueryMatch
+      }
+      if (!format && topic && author && source) {
+        return isTopicMatch && isAuthorMatch && isSourceMatch && isQueryMatch
       }
       if (!format && topic && !author && source) {
         return isTopicMatch && isSourceMatch && isQueryMatch
@@ -133,114 +162,177 @@ function Directory({ tutorials, formats, topics, authors, sources }) {
 
       // Remaining query combo
       if (!format && !topic && !author && !source) {
-        return isQueryMatch && isQueryMatch
+        return isQueryMatch
       }
     }
   })
 
   return (
-    <section>
+    <section className="">
       <h2 className="sr-only">Search for Gatsby JS tutorials</h2>
 
-      <form className="pv3">
-        <label className="mono">
-          <span className="pr2 fw7">Search:</span>
-          <input
-            ref={searchInput}
-            onChange={e => setQuery(e.target.value)}
-            type="text"
-            placeholder="Type here..."
-            className="f6"
-          />
-        </label>
-      </form>
-
-      {format && (
-        <div className="flex items-baseline pb3">
-          <p className="pr2 mono">
-            <span className="pr2 fw7">Format:</span>
-            <span className="lh-copy mono f6 black-60">{format}</span>
-          </p>
-          <button onClick={() => setFormat(null)} className="f6 mono">
-            [<span className="link">Clear filter</span>]
-          </button>
-        </div>
-      )}
-
-      {topic && (
-        <div className="flex items-baseline pb3">
-          <p className="pr2 mono">
-            <span className="pr2 fw7">Topic:</span>
-            <span className="lh-copy mono f6 black-60">{topic}</span>
-          </p>
-          <button onClick={() => setTopic(null)} className="f6 mono">
-            [<span className="link">Clear filter</span>]
-          </button>
-        </div>
-      )}
-
-      {author && (
-        <div className="flex items-baseline pb3">
-          <p className="pr2 mono">
-            <span className="pr2 fw7">Author:</span>
-            <span className="lh-copy mono f6 black-60">{author}</span>
-          </p>
-          <button onClick={() => setAuthor(null)} className="f6 mono">
-            [<span className="link">Clear filter</span>]
-          </button>
-        </div>
-      )}
-
-      {source && (
-        <div className="flex items-baseline pb3">
-          <p className="pr2 mono">
-            <span className="pr2 fw7">Source:</span>
-            <span className="lh-copy mono f6 black-60">{source}</span>
-          </p>
-          <button onClick={() => setSource(null)} className="f6 mono">
-            [<span className="link">Clear filter</span>]
-          </button>
-        </div>
-      )}
-
-      {(format || author || source || topic || query) && (
-        <p className="pb3 mono">
-          <span className="pr2 fw7">Results:</span>
-          <span className="lh-copy mono f6 black-60">
-            {filteredTutorials.length}
-          </span>
-        </p>
-      )}
-
-      {/* Tutorials matching search and filter parameters (if any) */}
-      <div className="directory-grid">
-        <Tutorials
-          tutorials={filteredTutorials}
-          setAuthor={setAuthor}
-          setSource={setSource}
-          setTopic={setTopic}
-        />
-
-        <aside className="bt b--black-10 pt4">
-          {/* Link to add a new tutorial */}
-          <Anchor
-            href="https://github.com/ooloth/gatsby-tutorials#how-do-i-add-a-tutorial"
-            className="link db mb4 md:f6 mono"
+      <Sticky>
+        {status => (
+          <div
+            className={`bg-near-white animate ${
+              status.status === Sticky.STATUS_FIXED ? `shadow pv2` : ``
+            }`}
           >
-            Add a tutorial
-            <Emoji
-              emoji="🙌"
-              ariaLabel="Emoji of two hands raised in appreciation"
-              className="emoji pl2"
-            />
-          </Anchor>
+            <div
+              className={`flex justify-between container w-100 animate ${
+                status.status === Sticky.STATUS_FIXED
+                  ? `items-baseline`
+                  : `items-end`
+              }`}
+            >
+              <form className="flex-auto">
+                <label className="flex mono">
+                  <span className="flex-none pr2 fw7">Search:</span>
+                  <input
+                    ref={searchInput}
+                    value={query}
+                    onChange={handleQuery}
+                    type="text"
+                    placeholder="Type here..."
+                    className="flex-auto f6"
+                  />
+                </label>
+              </form>
 
-          {/* Lists of all types, topics, authors and sources */}
-          <Formats formats={formats} currentFormat={format} setFormat={setFormat} />
-          <Topics topics={topics} currentTopic={topic} setTopic={setTopic} />
-          <Authors authors={authors} currentAuthor={author} setAuthor={setAuthor} />
-          <Sources sources={sources} currentSource={source} setSource={setSource} />
-        </aside>
+              <div className="flex items-end">
+                <TriggerAndOverlay
+                  formats={formats}
+                  topics={topics}
+                  authors={authors}
+                  sources={sources}
+                  currentFormat={format}
+                  currentTopic={topic}
+                  currentAuthor={author}
+                  currentSource={source}
+                  setFormat={setFormat}
+                  setTopic={setTopic}
+                  setAuthor={setAuthor}
+                  setSource={setSource}
+                  className="flex items-center md:dn ml3 br2 pv2 ph3 bg-purple tc white shadow animate hover:bg-blue"
+                />
+
+                {/* Link to add a new tutorial */}
+                <Anchor
+                  href="https://github.com/ooloth/gatsby-tutorials#how-do-i-add-a-tutorial"
+                  className="flex-none dn md:inline-flex ml3 br2 pv2 ph3 bg-purple lh-solid tc white shadow animate hover:bg-blue"
+                >
+                  <span>Add a tutorial</span>
+
+                  <Emoji
+                    emoji="🙌"
+                    ariaLabel="Emoji of two hands raised in appreciation"
+                    className="pl2"
+                    style={{ lineHeight: 0.75 }}
+                  />
+                </Anchor>
+              </div>
+            </div>
+          </div>
+        )}
+      </Sticky>
+
+      <div className="container pv3">
+        {/* {format && (
+          <div className="flex items-baseline pb3">
+            <p className="pr2 mono">
+              <span className="pr2 fw7">Format:</span>
+              <span className="lh-copy mono f6 black-60">{format}</span>
+            </p>
+            <button onClick={() => setFormat(null)} className="f6 mono">
+              [<span className="link">Clear filter</span>]
+            </button>
+          </div>
+        )} */}
+
+        {/* {topic && (
+          <div className="flex items-baseline pb3">
+            <p className="pr2 mono">
+              <span className="pr2 fw7">Topic:</span>
+              <span className="lh-copy mono f6 black-60">{topic}</span>
+            </p>
+            <button onClick={() => setTopic(null)} className="f6 mono">
+              [<span className="link">Clear filter</span>]
+            </button>
+          </div>
+        )} */}
+
+        {/* {author && (
+          <div className="flex items-baseline pb3">
+            <p className="pr2 mono">
+              <span className="pr2 fw7">Author:</span>
+              <span className="lh-copy mono f6 black-60">{author}</span>
+            </p>
+            <button onClick={() => setAuthor(null)} className="f6 mono">
+              [<span className="link">Clear filter</span>]
+            </button>
+          </div>
+        )} */}
+
+        {/* {source && (
+          <div className="flex items-baseline pb3">
+            <p className="pr2 mono">
+              <span className="pr2 fw7">Source:</span>
+              <span className="lh-copy mono f6 black-60">{source}</span>
+            </p>
+            <button onClick={() => setSource(null)} className="f6 mono">
+              [<span className="link">Clear filter</span>]
+            </button>
+          </div>
+        )} */}
+
+        {/* {(format || author || source || topic || query) && (
+          <p className="pb3 mono">
+            <span className="pr2 fw7">Results:</span>
+            <span className="lh-copy mono f6 black-60">
+              {filteredTutorials.length}
+            </span>
+          </p>
+        )} */}
+
+        {/* Tutorials matching search and filter parameters (if any) */}
+        <div className="directory-grid">
+          <Tutorials
+            tutorials={filteredTutorials}
+            currentFormat={format}
+            currentTopic={topic}
+            currentAuthor={author}
+            currentSource={source}
+            setFormat={setFormat}
+            setTopic={setTopic}
+            setAuthor={setAuthor}
+            setSource={setSource}
+            setQuery={setQuery}
+            searchInput={searchInput}
+          />
+
+          <aside className="dn md:db">
+            {/* Lists of all types, topics, authors and sources */}
+            <Formats
+              formats={formats}
+              currentFormat={format}
+              setFormat={setFormat}
+            />
+
+            <Topics topics={topics} currentTopic={topic} setTopic={setTopic} />
+
+            <Authors
+              authors={authors}
+              currentAuthor={author}
+              setAuthor={setAuthor}
+            />
+
+            <Sources
+              sources={sources}
+              currentSource={source}
+              setSource={setSource}
+            />
+          </aside>
+        </div>
       </div>
     </section>
   )
@@ -253,6 +345,7 @@ function Directory({ tutorials, formats, topics, authors, sources }) {
  */
 
 import React, { useState, useRef, useEffect } from 'react'
+import Sticky from 'react-stickynode'
 
 import Tutorials from '../components/Tutorials'
 import Anchor from '../components/Anchor'
@@ -261,5 +354,6 @@ import Formats from '../components/Formats'
 import Topics from '../components/Topics'
 import Authors from '../components/Authors'
 import Sources from '../components/Sources'
+import TriggerAndOverlay from '../components/TriggerAndOverlay'
 
 export default Directory
