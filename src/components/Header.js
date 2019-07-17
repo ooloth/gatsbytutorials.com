af355401518844097a4861b997be29a31e973e03
Michael Uloth <hello@michaeluloth.com>
2019-07-17 17:30:46 -0400

    refactor: upgrade graphql queries to use hooks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,46 @@
-const Header = ({ icon, count }) => (
-  <header className="bg-near-white pv4 sans-serif">
-    <div className="container pv2">
-      <div className="flex items-center" style={{ maxWidth: `32rem` }}>
-        <Img
-          fluid={icon.childImageSharp.fluid}
-          alt="GatsbyJS logo, consisting of a white 'G' on a purple circle"
-          className="flex-none w3 br-100 shadow"
-        />
+function Header() {
+  const icon = useGatsbyIcon()
+  const [tutorialsWithDates, tutorialsWithoutDates] = useTutorialsData()
+  const count = tutorialsWithDates.length + tutorialsWithoutDates.length
 
-        <h1 className="pl3 lh-copy f5 fw4">
-          <span className="fw7">Gatsby Tutorials</span> is a community-updated list
-          of
-          {` ${count}`}&nbsp;video, audio and written tutorials to help you learn{' '}
-          <Anchor href="https://www.gatsbyjs.org" className="link">
-            GatsbyJS
-          </Anchor>
-          {`. `}
-          <Emoji
-            emoji="👩‍💻"
-            ariaLabel="An emoji of a woman coding on a laptop"
-            className="emoji"
+  return (
+    <header className="bg-near-white pv4 sans-serif">
+      <div className="container pv2">
+        <div className="flex items-center" style={{ maxWidth: `32rem` }}>
+          <Img
+            fluid={icon.childImageSharp.fluid}
+            alt="GatsbyJS logo, consisting of a white 'G' on a purple circle"
+            className="flex-none w3 br-100 shadow"
           />
-        </h1>
-      </div>
-    </div>
-  </header>
-)
 
-/*
- *
- * Imports & Exports
- *
- */
+          <h1 className="pl3 lh-copy f5 fw4">
+            <span className="fw7">Gatsby Tutorials</span> is a community-updated
+            list of
+            {` ${count}`}&nbsp;video, audio and written tutorials to help you learn{' '}
+            <Anchor href="https://www.gatsbyjs.org" className="link">
+              GatsbyJS
+            </Anchor>
+            {`. `}
+            <Emoji
+              emoji="👩‍💻"
+              ariaLabel="An emoji of a woman coding on a laptop"
+              className="emoji"
+            />
+          </h1>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+///////////////////////////////////////////////////////////////////////////////////
 
 import React from 'react'
 
 import Anchor from '../components/Anchor'
 import Emoji from '../components/Emoji'
 import Img from '../components/Img'
+import useGatsbyIcon from '../queries/useGatsbyIcon'
+import useTutorialsData from '../queries/useTutorialsData'
 
 export default Header
---
+++ src/queries/useGatsbyIcon.js
@@ -0,0 +1,23 @@
+function useGatsbyIcon() {
+  const { icon } = useStaticQuery(
+    graphql`
+      query {
+        icon: file(relativePath: { eq: "images/favicon.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 80) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return icon
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+
+import { useStaticQuery, graphql } from 'gatsby'
+
+export default useGatsbyIcon
