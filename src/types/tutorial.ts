6a17125d7a0d4bdb2e2093446e78b2d57d093c84
Michael Uloth <hello@michaeluloth.com>
2019-10-06 20:07:05 -0400

    🍱 share Tutorial type

---
+++ src/types/tutorial.ts
@@ -0,0 +1,18 @@
+interface Tutorial {
+  title: string
+  link: string
+  formats: Array<string>
+  language: string
+  topics: Array<string>
+  date?: string
+  length?: string
+  authors?: Array<string>
+  source?: string
+  fields: {
+    authorsAsString: string
+    formatsAsString: string
+    topicsAsString: string
+  }
+}
+
+export default Tutorial
