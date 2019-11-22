54a66dfbe01080296aabee695de56a2d6063e5a8
Michael Uloth <hello@michaeluloth.com>
2019-11-22 17:43:20 -0500

    Update dependencies (#63)
    * 🛠 install dependabot
    
    * chore: update dependencies
    
    * 🔍 fix type exports
    
    * fix TS file extension

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,16 @@
-import Tutorial from './tutorial'
-
-export type Tutorial = Tutorial
+export interface Tutorial {
+  title: string
+  link: string
+  formats: Array<string>
+  language: string
+  topics: Array<string>
+  date?: string
+  length?: string
+  authors?: Array<string>
+  source?: string
+  fields: {
+    authorsAsString: string
+    formatsAsString: string
+    topicsAsString: string
+  }
+}
